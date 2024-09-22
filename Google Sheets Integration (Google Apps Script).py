28f6e2fe293c325d0f5872beee3fdfd5d909a52f
Brian masanja <brianaloyce@gmail.com>
2024-09-22 13:05:41 +0300

    Update Google Sheets Integration (Google Apps Script).py

--- Google Sheets Integration (Google Apps Script).py
+++ Google Sheets Integration (Google Apps Script).py
@@ -1,3 +1,10 @@
+'''To store the data into Google Sheets, you need to create a Google Apps Script that handles POST requests and stores data in your Google Sheet.
+
+Google Apps Script Code
+Go to your Google Sheet.
+Click on Extensions > Apps Script.
+Delete the placeholder code and add the following script:'''
+
 function doPost(e) {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   var data = JSON.parse(e.postData.contents);
